c20f4217132d84c0b8fbcb2f81ac504c57f9a302
Joao Nabosne <tibu0004@algonquinlive.com>
2019-03-29 16:56:19 -0400

    changes pizzas routes

--- routes/pizzas.js
+++ routes/pizzas.js
@@ -2,21 +2,46 @@
 const Pizza = require('../models/Pizza')
 const express = require('express')
 const router = express.Router()
-//ResourceNotFoundError
+
 
 router.get('/api/', async (req,res) => {
-
+    const pizzas = await Pizza.find()
+    res.send({ data: pizzas })
 })
 
 router.post('/api/', sanitizeBody, async (req,res) => {
-    
+    let newPizza = new Pizza(req.sanitizeBody)
+    try{
+        await newPizza.save()
+        res.atatus(201).send({ data: newPizza})
+    } catch (err) {
+        next(err)
+    }   
 })
 
 router.get('/api/:id', async (req,res) => {
-
+    try{
+        const pizzas = await Pizza.findById(req.params.id).populate('ingredients')
+        if(!pizzas)
+        throw new Error('Resource not found')
+        res.send({ data: pizzas })
+    } catch (err) {
+        sendResourceNotFound(req, res)
+    }
 })
 
-//const update
+const update = (overwrite = false) => async (req, res) => {
+    try{
+        const pizzas = await Pizza.findByIdAndUpdate(
+            req.params.id,
+            req.sanitizeBody,
+            {
+
+            }
+
+        )
+    }
+}
 
 router.put('/api/:id', sanitizeBody, update((overwrite = true)))
 router.put('/api/:id', sanitizeBody, update((overwrite = false)))
@@ -26,6 +51,6 @@ router.delete('/api/:id', async (req,res) => {
 })
 
 //add function sendResourceNotFound 
-//or use the request ResourceNotFoundError
+
 
 module.exports = router

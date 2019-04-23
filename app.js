f28ec957415227d6700368ccb4cc8b193951015b
Marcos <zorz0004@algonquinlive.com>
2019-04-23 13:38:43 -0400

    NPM CORS installed, app.js changed to use cors.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ require('./startup/database')()
 
 const compression = require('compression')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(compression())
+app.use(cors())
 app.use(express.json())
 app.use(require('express-mongo-sanitize')())
 
